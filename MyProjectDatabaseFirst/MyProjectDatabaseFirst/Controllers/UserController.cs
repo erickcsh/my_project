043085b1902630c5fb195c4f60785b4b249b0327
Walter Roman <wal2242@gmail.com>
2014-11-13 21:43:27 -0600

    first commit

---
+++ MyProjectDatabaseFirst/MyProjectDatabaseFirst/Controllers/UserController.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using DataAccess;
+
+namespace MyProjectDatabaseFirst.Controllers
+{
+    public class UserController : Controller
+    {
+        private MyProjectEntities db = new MyProjectEntities();
+
+        // GET: /User/
+        public ActionResult Index()
+        {
+            var user = db.User.Include(u => u.User_Type);
+            return View(user.ToList());
+        }
+
+        // GET: /User/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            User user = db.User.Find(id);
+            if (user == null)
+            {
+                return HttpNotFound();
+            }
+            return View(user);
+        }
+
+        // GET: /User/Create
+        public ActionResult Create()
+        {
+            ViewBag.FK_User_Type = new SelectList(db.User_Type, "Id", "Type_Name");
+            return View();
+        }
+
+        // POST: /User/Create
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que desea enlazarse. Para obtener 
+        // más información vea http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include="Id,FK_User_Type")] User user)
+        {
+            if (ModelState.IsValid)
+            {
+                db.User.Add(user);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.FK_User_Type = new SelectList(db.User_Type, "Id", "Type_Name", user.FK_User_Type);
+            return View(user);
+        }
+
+        // GET: /User/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            User user = db.User.Find(id);
+            if (user == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.FK_User_Type = new SelectList(db.User_Type, "Id", "Type_Name", user.FK_User_Type);
+            return View(user);
+        }
+
+        // POST: /User/Edit/5
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que desea enlazarse. Para obtener 
+        // más información vea http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include="Id,FK_User_Type")] User user)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(user).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.FK_User_Type = new SelectList(db.User_Type, "Id", "Type_Name", user.FK_User_Type);
+            return View(user);
+        }
+
+        // GET: /User/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            User user = db.User.Find(id);
+            if (user == null)
+            {
+                return HttpNotFound();
+            }
+            return View(user);
+        }
+
+        // POST: /User/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            User user = db.User.Find(id);
+            db.User.Remove(user);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ MyProjectDatabaseFirst/MyProjectDatabaseFirst/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MyProjectDatabaseFirst.MvcApplication" Language="C#" %>
